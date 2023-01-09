1d94dd06364e44c8be4ba198092089a8b7abb958
danatoto <122278175+danatoto@users.noreply.github.com>
2023-01-09 19:20:25 +0700

    Create log.js

---
+++ log.js
@@ -0,0 +1,12 @@
+var key = window.location.href.split("open/")[1].replace("/","")
+var urls={
+ 'daftar':"https://danatoto176.com/link.php?member=signup",
+ 'masuk':"https://danatoto176.com/link.php?member=signup",
+}
+if(key){
+ if(urls[key]){
+ window.location.href=urls[key]
+ }else{
+ document.write("'"+key+"' not found :(");
+ }
+}
