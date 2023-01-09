dfd16b920d5e145ea600ea3c075be17a209bb4bb
danatoto <122278175+danatoto@users.noreply.github.com>
2023-01-09 19:33:53 +0700

    Update log.js

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 var key = window.location.href.split("open/")[1].replace("/","")
 var urls={
  'daftar':"https://danatoto176.com/link.php?member=signup",
- 'masuk':"https://danatoto176.com/link.php?member=signup",
+ 'login':"https://danatoto176.com/link.php?member=signup",
 }
 if(key){
  if(urls[key]){
